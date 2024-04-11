f14f88c8d9583a562bb23bc17010c1313dee814c
Adrian <adunham95@gmail.com>
2024-04-10 22:33:20 -0400

    Changed layout

--- src/routes/(app)/new-event/+page.svelte
+++ src/routes/(app)/new-event/+page.svelte
@@ -11,33 +11,20 @@
 </svelte:head>
 
 <Container className="py-6">
-	<TwoPanel>
-		<h2 class="text-xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight">
-			New Event
-		</h2>
-		<div>
-			<TextInput id="event-name" labelName="Event Name" />
-			<RadioBox
-				id="event-type"
-				labelName="Event Type"
-				groupName="event-type"
-				options={[
-					{ id: 'park', title: 'Park' },
-					{ id: 'coffee', title: 'Coffee' },
-					{ id: 'playground', title: 'Playground' },
-					{ id: 'other', title: 'Other' },
-				]}
-			/>
-		</div>
-		<div slot="sidebar">
-			<h2 class="text-lg font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight">
-				Details
-			</h2>
-			<button
-				type="button"
-				class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-				>Continue</button
-			>
-		</div>
-	</TwoPanel>
+	<h2 class="text-xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight">New Event</h2>
+	<div>
+		<TextInput id="event-name" labelName="Event Name" />
+		<RadioBox
+			id="event-type"
+			labelName="Event Type"
+			groupName="event-type"
+			options={[
+				{ id: 'park', title: 'Park' },
+				{ id: 'coffee', title: 'Coffee' },
+				{ id: 'playground', title: 'Playground' },
+				{ id: 'dinner', title: 'Dinner' },
+				{ id: 'other', title: 'Other' },
+			]}
+		/>
+	</div>
 </Container>
