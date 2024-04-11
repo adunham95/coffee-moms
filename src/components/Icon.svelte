58f7e80f8c66f7fc38b0eec7cb5ebe6154495733
Adrian <adunham95@gmail.com>
2024-04-11 18:58:24 -0400

    Working on llanding page

---
+++ src/components/Icon.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	export let name: keyof typeof icons;
+	export let width = '1rem';
+	export let height = '1rem';
+	export let fill = 'none';
+	export let stroke = 'currentColor';
+	export let strokeWidth = 1.5;
+	export let focusable: string | number | null | undefined = undefined;
+	let icons = {
+		check: {
+			box: 24,
+			svg: `<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />`,
+		},
+		chat: {
+			box: 24,
+			svg: `<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+					/>`,
+		},
+		calender: {
+			box: 24,
+			svg: `<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+					/>`,
+		},
+		stars: {
+			box: 24,
+			svg: `<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+					/>`,
+		},
+	} as const;
+	let displayIcon = icons[name];
+</script>
+
+<svg
+	class={$$props.class}
+	{focusable}
+	{width}
+	{height}
+	{fill}
+	{stroke}
+	stroke-width={strokeWidth}
+	viewBox="0 0 {displayIcon.box} {displayIcon.box}">{@html displayIcon.svg}</svg
+>
---
+++ src/routes/(marketing)/FeatureList.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Icon from '../../components/Icon.svelte';
+	export let features: { title: string; description: string; icon: string }[] = [];
+</script>
+
+<dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+	{#each features as feature}
+		<div class="relative pl-16">
+			<dt class="text-base font-semibold leading-7 text-gray-900">
+				<div
+					class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-brand-600"
+				>
+					<Icon class="h-6 w-6 text-white" name={feature.icon} />
+				</div>
+				{feature.title}
+			</dt>
+			<dd class="mt-2 text-base leading-7 text-gray-600">
+				{feature.description}
+			</dd>
+		</div>
+	{/each}
+</dl>
