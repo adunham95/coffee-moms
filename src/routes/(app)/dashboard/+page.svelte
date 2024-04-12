e5d51a523a515ba7c67faadf8ed3a79b2661525f
Adrian <adunham95@gmail.com>
2024-04-11 20:09:41 -0400

    Working on dashboard

---
+++ src/routes/(app)/dashboard/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Container from '../../../components/Container.svelte';
+	import EventList from './EventList.svelte';
+</script>
+
+<svelte:head>
+	<title>Dashboard | Coffee Mom</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<Container className="py-10">
+	<EventList />
+</Container>
